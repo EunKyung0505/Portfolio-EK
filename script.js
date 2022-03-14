1bf9f9de187fc977adb9480f20e20335555d81ed
Eunkyung Park <apjp0505@gmail.com>
2022-03-14 16:11:14 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const totalBoxes = document.querySelectorAll('div.color').length;
+  let totalColored = 0;
+
+  function randomColor() {
+    const rNum = () => (Math.floor(Math.random() * 255));
+
+    // return 'rgb(' + Math.floor(Math.random() * 255)+ ','
+    // + Math.floor(Math.random() * 255) + ','
+    // + Math.floor(Math.random()*255) + ')';
+    return `rgb(${rNum()}, ${rNum()}, ${rNum()})`;
+  }
+
+  function bgChange(e) {
+
+    if (!e.target.dataset.colored && e.target.classList.contains('color')) {
+      const color = document.getElementById('color-picker').value;
+      
+      if(color === "") {
+        e.target.style.backgroundColor = randomColor();
+        e.target.dataset.colored = true;
+      } else {
+        e.target.style.backgroundColor = color;
+        e.target.dataset.colored = true;
+        document.getElementById('color-picker').value = "";       
+      }
+      totalColored++;
+      e.target.textContent = e.target.style.backgroundColor;
+
+      if (document.querySelectorAll('[data-colored]').length
+        === document.querySelectorAll('.color').length) {
+          setTimeout(() => {
+            alert("all boxes colored!");  
+          }, 0);
+        }
+      }
+  }
+
+  document.getElementById('boxes').addEventListener('click', bgChange);
+});
